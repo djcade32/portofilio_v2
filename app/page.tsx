ea11f5ac2d971d3b8efc91a740dd17ddfe7d9c05
Norman Cade <norman_cade32@yahoo.com>
2026-02-14 07:46:13 -0500

    Create Footer section

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import FeaturedWork from "@/sections/FeaturedWork";
 import Hero from "@/sections/Hero";
 import Tech from "@/sections/Tech";
 import Contact from "@/sections/Contact";
+import Footer from "@/sections/Footer";
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
       <FeaturedWork />
       <Experience />
       <Contact />
+      <Footer />
     </main>
   );
 }
