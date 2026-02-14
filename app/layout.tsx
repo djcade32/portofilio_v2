5430ee8116ca27f49702bab1f36894ee266bc955
Norman Cade <norman_cade32@yahoo.com>
2026-02-14 18:51:40 -0500

    Keep mobile pull refresh animation

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="overscroll-y-none">
       <body className={`${outfit.variable} ${spaceGrotesk.variable} antialiased`}>
         <Navbar />
         <main>
